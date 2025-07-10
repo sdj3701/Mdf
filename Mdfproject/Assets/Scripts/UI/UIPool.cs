b37076211e2dcc06ffc6bd4f59a687d019233395
shindongju <sdj3701@naver.com>
2025-07-10 11:39:01 +0900

    Feat : addresables를 추가 진행중
    원래는 Pool에서 Instantiate을 사용해서 생성하는 코드를 사용했는데 변경 중

--- Mdfproject/Assets/Scripts/UI/UIPool.cs
+++ Mdfproject/Assets/Scripts/UI/UIPool.cs
@@ -28,10 +28,24 @@ public class UIPool : MonoBehaviour
         }
         else
         {
-            // 이름에 해당하는 UI 오브젝트가 풀에 없으면 새로 생성하여 반환
-            // TODO : addressable로 변경 해야함
+            return AddGetObject(name);
+        }
+    }
+
+    // UI 요소가 없으면 새로 생성하여 반환
+    public GameObject AddGetObject (string name)
+    {
+        // 혹시 있으면 그냥 풀에서 꺼내쓰고
+        if (pool.ContainsKey(name))
+        {
+            GameObject obj = pool[name];
+            obj.SetActive(true);  // 활성화 상태로 반환
+            return obj;
+        }
+        else
+        {
+            // 없으면 Addressable로 불러오기
             GameObject obj = Object.Instantiate(prefab);
-            obj.name = name;  // 이름 설정
             return obj;
         }
     }
